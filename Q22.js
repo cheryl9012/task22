6c8b4a77aed614880b3b6e4d79a5ad2ff86f0e1d
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:31:32 +0500

    Add files via upload

---
+++ Q22.js
@@ -0,0 +1,7 @@
+"use strict";
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let programming_language = ["Html", "Css", "Javascript", "Typecsript", "Python"];
+// intentional error: Array are zero-indexed, so index 3 is out of bonds
+console.log(programming_language[5]);
+// correct the error
+console.log(programming_language[4]);
